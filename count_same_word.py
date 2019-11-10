0ee56fcf27b505bf05ae3a3e4ca5e0a27bb0ca20
atifkarim <atif0902082@gmail.com>
2019-11-10 10:12:06 +0100

    from a given sentence count the same word.

---
+++ count_same_word.py
@@ -0,0 +1,11 @@
+import numpy as np
+import collections
+
+my_var = 'i know him and i and she are best friend of him to know the thing which we do not know'
+
+words = my_var.split(' ')
+print(words)
+
+word_counts = collections.Counter(words)
+for word, count in sorted(word_counts.items()):
+    print('"%s" is repeated %d time%s.' % (word, count, "s" if count > 1 else ""))
