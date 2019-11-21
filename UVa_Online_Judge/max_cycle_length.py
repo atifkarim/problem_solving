64d7e4bb14a0c4decbf69e1dbc13063e85715b1a
atifkarim <atif0902082@gmail.com>
2019-11-22 00:19:13 +0100

    done solving

--- UVa_Online_Judge/max_cycle_length.py
+++ UVa_Online_Judge/max_cycle_length.py
@@ -9,15 +9,14 @@ final_count = 0
 
 def cycle_length(x, count_even, count_odd, final_count):
     if x == 1:
-        print('TERMINATE ----------, I am now: ',x,' so I should stop')
+        # print('TERMINATE ----------, I am now: ',x,' so I should stop')
         count_even = count_even
         count_odd = count_odd
-        print('In this case count_even: ', count_even,' and count_odd: ', count_odd)
+        # print('In this case count_even: ', count_even,' and count_odd: ', count_odd)
         final_count = count_even+count_odd
-        print('TERMINATION ZONE')
-        print('final count: ', final_count)
+        # print('TERMINATION ZONE')
+        # print('final count: ', final_count)
     elif x%2 == 0:
-        # print('hi now I am even: ',x)
         count_even = count_even+1
         # print('count even: ', count_even)
         x = int(x/2)
@@ -25,12 +24,12 @@ def cycle_length(x, count_even, count_odd, final_count):
         if x !=1:
             count_even, count_odd, final_count = cycle_length(x,count_even, count_odd, final_count)
         elif x==1:
-            print('SORRY ----------, I am now: ', x, ' so I should stop')
+            # print('SORRY ----------, I am now: ', x, ' so I should stop')
             count_even = count_even
             count_odd = count_odd
-            print('In this case count_even: ', count_even, ' and count_odd: ', count_odd)
+            # print('In this case count_even: ', count_even, ' and count_odd: ', count_odd)
             final_count = count_even + count_odd+1
-            print('final count: ', final_count)
+            # print('final count in even: ', final_count)
             return count_even, count_odd, final_count
 
 
@@ -43,12 +42,12 @@ def cycle_length(x, count_even, count_odd, final_count):
             count_even, count_odd, final_count = cycle_length(x,count_even, count_odd, final_count)
         elif x ==1:
             count_odd = count_odd
-            print('SORRY ----------, I am now: ', x, ' so I should stop')
+            # print('SORRY ----------, I am now: ', x, ' so I should stop')
             count_even = count_even
             count_odd = count_odd
-            print('In this case count_even: ', count_even, ' and count_odd: ', count_odd)
+            # print('In this case count_even: ', count_even, ' and count_odd: ', count_odd)
             final_count = count_even+count_odd+1
-            print('final count: ', final_count)
+            # print('final count in odd: ', final_count)
             return count_even, count_odd, final_count
 
 
@@ -56,17 +55,33 @@ def cycle_length(x, count_even, count_odd, final_count):
 
 
 # final_even, final_odd, final_count_1 = cycle_length(22, count_even, count_odd, final_count)
+if first_num<second_num:
+    stepsize = 1
+    second_num_1 = second_num+1
+else:
+    stepsize = -1
+    second_num_1 = second_num-1
+
+# for k in range(6,5-1,-1):
+#     print(k)
+
 maximal_length_array = []
-for i in range (first_num,second_num+1):
+print(type(maximal_length_array))
+maximal_length_array = np.array(maximal_length_array)
+
+print('step size: ', stepsize)
+for i in range (first_num,second_num_1, stepsize):
+    # print('code is exuting for: ',i)
     final_even, final_odd, final_count_1 = cycle_length(i, count_even, count_odd, final_count)
-    print('!!!!!!!!!!!!!!!',final_count_1)
-    maximal_length_array.append(final_count_1)
+    # print('!!!!!!!!!!!!!!!',final_count_1)
+    # maximal_length_array.append(final_count_1)
+    maximal_length_array = np.append(maximal_length_array, final_count_1)
 
-len(maximal_length_array)
-maximal_length_array
-maximal_length_array_array = np.array(maximal_length_array)
-max_len = maximal_length_array_array.max()
-print('max_len: ',max_len)
+# len(maximal_length_array)
+# print('array: ',maximal_length_array)
+# maximal_length_array_array = np.array(maximal_length_array)
+max_len = int(maximal_length_array.max())
+# print('max_len: ',max_len)
 
-print('*'*10,' Desired output ','*'*10)
+# print('*'*10,' Desired output ','*'*10)
 print(first_num,' ', second_num,' ',max_len)
