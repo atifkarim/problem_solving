a264fbc33033f1411286d8d047e9a35d3fcfb035
atifkarim <atif0902082@gmail.com>
2019-11-29 11:33:05 +0100

    tried several approach to get the value of button of keyboard

--- interrupt_code.py
+++ interrupt_code.py
@@ -24,33 +24,57 @@ print('time number greater than 0 to run the code: ')
 #     except:
 #         break
 
-
-import curses
-from datetime import datetime
-import time
+######################### get the key charecter #########################
+# import curses
+# from datetime import datetime
+# import time
 # now = datetime.now()
 # current_time = now.strftime("%H:%M:%S")
-# print"Current Time =", current_time
-
-
+# print("Current Time =", current_time)
+#
+#
+#
+#
 # def main():
 #     stdscr = curses.initscr()
 #     while True:
 #         now = datetime.now()
 #         current_time = now.strftime("%H:%M:%S")
 #         print("Current Time =", current_time)
-#         time.sleep(2)
+#         # time.sleep(2)
 #         key = stdscr.getch()
 #         if key == 27: # This is the escape key code
 #              curses.endwin()
 #              break
 #
 # main()
-
-# import getch
+##############################################################################################
+import getch
 # char = getch.getch()
 # print('char is: ', char)
-#########################################################################
+##############################################################################################
+# import cv2
+#
+# while True:
+#     key = cv2.waitKey(1) & 0xFF
+#     # print('key: ',key)
+#
+#     # if the 'ESC' key is pressed, Quit
+#     if key == 27:
+#         quit()
+#     if key == 0:
+#         print ("up")
+#     elif key == 1:
+#         print ("down")
+#     elif key == 2:
+#         print ("left")
+#     elif key == 3:
+#         print ("right")
+#     # 255 is what the console returns when there is no key press...
+#     elif key != 255:
+#         print('I am here???')
+#         print(key)
+######################### press Q will QUIT, press U will CONTINUE #########################
 # import sys
 #
 # while True:
@@ -63,10 +87,62 @@ import time
 #         print("User")
 #         #do_something()
 
-import time
-try:
-    while True:
-        print('hi run')
-        time.sleep(2)
-except KeyboardInterrupt:
-    pass
+
+
+######################### interrupt code with ctrl+C #########################
+# import time
+# try:
+#     while True:
+#         print('hi run')
+#         time.sleep(2)
+# except KeyboardInterrupt:
+#     pass
+
+
+###########################################################################################
+# print(ord('\x1b'))
+# print(ord('.'))
+# print(type(ord('.')))
+# chr(27)
+###########################################################################################
+# # import msvcrt # it is not for linux, only for windows
+# import getch
+# char = getch.getch()
+# print(char)
+# print('ord in char: ',ord(char))
+######################## it will tell the key name. #################################
+# import curses
+# import os
+#
+# def main(win,char):
+#     win.nodelay(True)
+#     key=""
+#     win.clear()
+#     win.addstr("Detected key:")
+#     while 1:
+#         try:
+#            key = win.getkey()
+#            win.clear()
+#            win.addstr("Detected key:")
+#            win.addstr(str(key))
+#            # char = getch.getch(win.addstr(str(key)))
+#            # print('char: ',char)
+#            if key == os.linesep:
+#               break
+#         except Exception as e:
+#            # No input
+#            pass
+# char = None
+# curses.wrapper(main,char)
+
+
+################################## do task if press button ##################################
+import getch
+while True:
+    char = getch.getch()
+    key_val = ord(char)
+    print('key_val: ',key_val,' type: ',type(key_val))
+    if key_val == 46:
+        print('I am doing job as the key val is: ',key_val)
+    else:
+        print('I have to quit as the key val is: ',key_val)
