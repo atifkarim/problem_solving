9d4e2daadb6bc6e1847c8a5fcf585de7c0a22cb6
atifkarim <atif0902082@gmail.com>
2019-12-03 10:28:10 +0100

    problem?

---
+++ UVa_Online_Judge/max_binary_gap.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+num = '10001'
+num_split = np.split()
+print(num_split)
+# print(type(num))
+#
+# for i in num:
+#     print(i)
+
+# print('length of num: ',len(num))
+zero_count =0
+for index, val in enumerate(num):
+    print(num[index])
+    if val == '0':
+        pass
+    elif val == '1':
+        if val
+
+
+def max_binary_gap(number, count_zero):
+    if number == '0':
+        count_zero+=1
+    eli
+
