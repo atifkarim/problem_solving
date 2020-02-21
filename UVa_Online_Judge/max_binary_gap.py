9d2e67e38afd742e8518b189fc9ed48f437341cc
atifkarim <atif0902082@gmail.com>
2020-02-22 00:59:11 +0100

    done cyclic rotation though has some error

--- UVa_Online_Judge/max_binary_gap.py
+++ UVa_Online_Judge/max_binary_gap.py
@@ -1,82 +1,49 @@
-# import numpy as np
-#
-# num = '10001'
-# # num_split = np.split()
-# # print(num_split)
-# # print(type(num))
-# #
-# # for i in num:
-# #     print(i)
-#
-# # print('length of num: ',len(num))
-# # zero_count =0
-# # for index, val in enumerate(num):
-# #     print(num[index])
-# #     if val == '0':
-# #         pass
-# #     elif val == '1':
-# #         if val
-# #
-# #
-# # def max_binary_gap(number, count_zero):
-# #     if number == '0':
-# #         count_zero+=1
-# #     eli
-#
-# a='1'
-# b='0'
-# count=0
-# long_count = 0
-#
-#
-# A = []
-# A = np.array(A)
-# print(type(A))
-#
-# for i in num:
-#     A = np.append(i,A)
-#
-#
-# print(A)
-#
-# for i in A:
-#     if i == a:
-#         count+=1
-#
-#
+print('given input is: ')
+a = int(input())
 
+def max_binary_gap(N):
+    binary_val = format(N, "08b")
+    string_val = str(binary_val)
+    A = []
 
-# print(n,' **',type(n))
+    for i in string_val:
+        A.append(i)
 
-
-# def binary_gap(N):
-#     return len(max(format(N, 'b').strip('0').split('1')))
-#
-# # n = ('input is: ',int(input()))
-#
-# n = int(input())
-# a = binary_gap(n)
-# print('max length is: ',a)
-
-def solution(N):
-    max_gap = 0
-    current_gap = 0
-    # Skip the tailing zero(s)
-    while N > 0 and N%2 == 0:
-        N //= 2
-    while N > 0:
-        remainder = N%2
-        if remainder == 0:
-            # Inside a gap
-            current_gap += 1
+    for idx, val in enumerate(A):
+        if A[0] == '0':
+            A = A[1:]
         else:
-            # Gap ends
-            if current_gap != 0:
-                max_gap = max(current_gap, max_gap)
-                current_gap = 0
-        N //= 2
-    return max_gap
+            A = A[0:]
+            break
 
-n = int(input())
-a = solution(n)
-print('max length is: ',a)
+    A = A[::-1]
+
+    for idx, val in enumerate(A):
+        if A[0] == '0':
+            A = A[1:]
+        else:
+            A = A[0:]
+            break
+
+    if 2>1:
+        count = 0
+        gap = 0
+
+        if len(A)==1:
+            pass
+        else:
+            for idx, val in enumerate(A):
+                if A[idx] == '0':
+                    count += 1
+                else:
+                    if count > gap:
+                        gap = count
+                        count = 0
+                    else:
+                        gap = gap
+                        count=0
+
+    return gap
+
+binary_gap = max_binary_gap(a)
+print('binary gap is: ',binary_gap)
