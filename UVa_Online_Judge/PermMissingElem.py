c9da20b860f5b85b2d437c8975959a233b8b0ba4
atifkarim <atif0902082@gmail.com>
2020-02-23 00:10:31 +0100

    code modified , got 100 percent accuracy

--- UVa_Online_Judge/PermMissingElem.py
+++ UVa_Online_Judge/PermMissingElem.py
@@ -1,52 +1,26 @@
-# A.sort()
-# next_val = 0
-# for idx,val in enumerate(A):
-#     next_val = A[idx]+1
-#     if next_val!=A[idx+1]:
-#         print('missing val is: ',next_val)
-#         break
-#     else:
-#         pass
+# https://app.codility.com/demo/results/trainingF2SB5M-5TC/
 
-
-# def solution(A):
-#     A.sort()
-#     next_val = 0
-#     for idx, val in enumerate(A):
-#         next_val = A[idx] + 1
-#         if next_val != A[idx + 1]:
-#             return next_val
-#             break
-#         else:
-#             pass
 
 def solution(A):
     A.sort()
-    # missing_val = 0
-    # print('len of A: ',len(A))
-    # print(A)
+
+
+    while A==[]:
+        return 1
+        break
+    while A[0] !=1 :
+        return 1
+        break
     for idx, val in enumerate(A):
-        print('here idx: ',idx,'*'*10,A)
-        if A==[]:
-            return 1
-        elif A[0]!=1:
-            return 1
-        elif len(A)==1:
+
+
+        if len(A)==1:
             return A[-1]+1
         else:
-            print('idx: ',idx,' A[idx]+1: ',A[idx]+1,' A[idx+1]: ',A[idx+1])
-            if idx<len(A)-1 and (A[idx]+1) != A[idx + 1]:
-                return (A[idx]+1)
-                break
+
+            if idx<len(A)-1:
+                if (A[idx]+1) != A[idx + 1]:
+                    return (A[idx]+1)
+                    break
             else:
                 return A[-1]+1
-
-
-print(solution([]))
-
-# C = []
-# print(C)
-#
-# len(C)
-#
-# len(C.sort())
