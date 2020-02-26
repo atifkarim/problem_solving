067f462cecc744a51f1b2907642c648783179554
atifkarim <atif0902082@gmail.com>
2020-02-26 05:00:48 +0100

    modified

--- sewts_ws/src/sewts_task/scripts/action_server.py
+++ sewts_ws/src/sewts_task/scripts/action_server.py
@@ -13,14 +13,12 @@ print "path: ",os.getcwd()
 with open('/home/atif/sewts_ws/src/sewts_task/scripts/product.json', 'r') as f:
   config = json.load(f)
 
-#from sewts_task.msg import DoDishesAction
-
-class DoDishesServer:
-  _result = DoDishesResult()
-  _feedback= DoDishesFeedback()
+class ProductSearchServer:
+  _result = ProductSearchResult()
+  _feedback= ProductSearchFeedback()
 
   def __init__(self):
-    self.server = actionlib.SimpleActionServer('do_dishes', DoDishesAction, self.execute, False)
+    self.server = actionlib.SimpleActionServer('product', ProductSearchAction, self.execute, False)
     self.server.start()
     self.found = None
     self.product_container = None
@@ -39,9 +37,7 @@ class DoDishesServer:
 
   def search_product(self,goal_val):
     self.product_container = self.create_product_container()
-    #print "dict here !!!!!!!!!!!!",self.product_container
     for y in self.product_container.keys():
-      print "herererere type ----------",type(goal_val)," printing the key of dict",y
       if int(y)==goal_val:
         return True
       else:
@@ -49,115 +45,33 @@ class DoDishesServer:
     return False
 
   def execute(self, goal):
-    #print "printing goal: ",goal.dishwasher_id
-    #print "printing goal for 2: ",goal.dishwasher_id1
-	  # Do lots of awesome groundbreaking robot stuff here
-	  # #var_result
     if goal.goal_check_product!=0:
-      #var_result = True
-		  # goal.dishwasher_id=None
-      #print "i am here"
-      #self._feedback.percent_complete=24.00
-      #print "feedback in py: ",self._feedback.percent_complete 
       goal_val = goal.goal_check_product
-      print "goal val: ",goal_val
       var_result = self.search_product(goal_val)
       self.found=str(goal.goal_check_product)
-      print "nnnn: ",self.found," status: ",var_result
       self._result.result_presence_of_product=var_result
-	#self._result.total_dishes_cleaned=15.00
-	#print "printed already: ",self._feedback.percent_complete
-      #self.server.publish_feedback(self._feedback)
       self.server.set_succeeded(self._result)
-	#return 0
-    
-    #self.server.publish_feedback(self._feedback)
-    #self.server.set_succeeded(self._feedback)
-      #self.server.set_succeeded(self._result)
 
     elif goal.goal_check_product_description!=0:
-	print "goal here: ",goal.goal_check_product_description
 	if goal.goal_check_product_description==1:
-	    #goal.goal_check_product_description=None
 	    price = self.product_container[self.found][0]
 	    self._result.result_product_description=price
-	    print "price of product: ",self._result.result_product_description
+	    print "price of product: ",self._result.result_product_description,"\n"
 	    self.server.set_succeeded(self._result)
 	elif goal.goal_check_product_description==2:
-#goal.dishwasher_id1=None
 	    weight = self.product_container[self.found][1]
 	    self._result.result_product_description=weight
-	    print "weight of product: ",self._result.result_product_description
+	    print "weight of product: ",self._result.result_product_description,"\n"
 	    self.server.set_succeeded(self._result)
         else:
 	    self._result.result_product_description=0
-	    print "here py wrong"
+	    print "wrong button of product description is pressed","\n"
 	    self.server.set_succeeded(self._result)
     return 0
 
 
 
 if __name__ == '__main__':
-  rospy.init_node('do_dishes_server')
-  server = DoDishesServer()
+  rospy.init_node('product_server')
+  server = ProductSearchServer()
   rospy.spin()
-
-
-
-'''----------------------------------
-
-
-#! /usr/bin/env python
-
-import rospy
-
-import actionlib
-
-import actionlib_tutorials.msg
-
-class FibonacciAction(object):
-    # create messages that are used to publish feedback/result
-    _feedback = actionlib_tutorials.msg.FibonacciFeedback()
-    _result = actionlib_tutorials.msg.FibonacciResult()
-
-    def __init__(self, name):
-        self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.FibonacciAction, execute_cb=self.execute_cb, auto_start = False)
-        self._as.start()
-      
-    def execute_cb(self, goal):
-        # helper variables
-        r = rospy.Rate(1)
-        success = True
-        
-        # append the seeds for the fibonacci sequence
-        self._feedback.sequence = []
-        self._feedback.sequence.append(0)
-        self._feedback.sequence.append(1)
-        
-        # publish info to the console for the user
-        rospy.loginfo('%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
-        
-        # start executing the action
-        for i in range(1, goal.order):
-            # check that preempt has not been requested by the client
-            if self._as.is_preempt_requested():
-                rospy.loginfo('%s: Preempted' % self._action_name)
-                self._as.set_preempted()
-                success = False
-                break
-            self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
-            # publish the feedback
-            self._as.publish_feedback(self._feedback)
-            # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-            r.sleep()
-          
-        if success:
-            self._result.sequence = self._feedback.sequence
-            rospy.loginfo('%s: Succeeded' % self._action_name)
-            self._as.set_succeeded(self._result)
-        
-if __name__ == '__main__':
-    rospy.init_node('fibonacci')
-    server = FibonacciAction(rospy.get_name())
-    rospy.spin()'''
--- sewts_ws/src/sewts_task/src/action_client.cpp
+++ sewts_ws/src/sewts_task/src/action_client.cpp
@@ -1,83 +1,9 @@
-/*
-#include <sewts_task/DoDishesAction.h> // Note: "Action" is appended
-#include <actionlib/client/simple_action_client.h>
-using namespace std;
-using namespace sewts_task;
-typedef actionlib::SimpleActionClient<sewts_task::DoDishesAction> Client;
-
-
-/*void feedbackCb(const DoDishesFeedbackConstPtr& feedback)
-{
-  ROS_INFO("Got Feedback of length %f", feedback->percent_complete);
-}
-///////
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "do_dishes_client");
-  Client client("do_dishes", true); // true -> don't need ros::spin()
-  client.waitForServer();
-  sewts_task::DoDishesGoal goal;
-  sewts_task::DoDishesResult result;
-  //sewts_task::DoDishesFeedback feedback;
-  unsigned int d;
-  float s,fed;
-  
-  
-// Fill in goal here
-  std::cout<<"You can search about 3 products and each of them unique identity marked with 1,2 and 3. Please enter your desired ID\n";  
-  std::cout<<"Enter your ID: ";
-  std::cin>>d;
-  goal.dishwasher_id=d;
-  client.sendGoal(goal);
-  //client.sendGoal(goal,feedback=feedback_cb);
-  client.waitForResult(ros::Duration(5.0));
-  client.getResult();
-  DoDishesFeedbackConstPtr feedback;
-  
-  //ROS_INFO("Got Feedback of length %f", &feedback->percent_complete);
-  //&feedbackCb(feedback);
-  //client.feedback_cb_();
-  //s=feedback.percent_complete;
-  s=client.getResult()->total_dishes_cleaned;
-  //fed=client.getResult()->percent_complete;
-  //fed=client.feedback_cb_()->percent_complete;
-  //std::cout<<"fed result  "<< fed <<std::endl;
-  std::cout<<"got result  "<< s <<std::endl;
-  if (s == 15.0)
-    printf("Ok\n");
-  ros::spin();
-  if (s != 15.0)
-    printf("check the type");
-  if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    printf("Yay! The dishes are now clean");
-  printf("Current State: %s\n", client.getState().toString().c_str());
-  return 0;
-}
-*/
-
-
-
-
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
-#include <sewts_task/DoDishesAction.h> 
+#include <sewts_task/ProductSearchAction.h> 
 
 using namespace sewts_task;
-typedef actionlib::SimpleActionClient<sewts_task::DoDishesAction> Client;
-
-// Called once when the goal completes
-/*oid doneCb(const actionlib::SimpleClientGoalState& state,
-            const DoDishesResultConstPtr& result)
-{
-  ROS_INFO("Finished in state [%s]", state.toString().c_str());
-  ROS_INFO("Answer: %i", result->sequence.back());
-  ros::shutdown();
-}
-
-*///////////////
-
-// Called once when the goal becomes active
+typedef actionlib::SimpleActionClient<sewts_task::ProductSearchAction> Client;
 
 void activeCb()
 {
@@ -85,10 +11,7 @@ void activeCb()
 }
 
 
-
-// Called every time feedback is received for the goal
-
-float feedbackCb(const DoDishesFeedbackConstPtr& feedback)
+float feedbackCb(const ProductSearchFeedbackConstPtr& feedback)
 {
   float r = feedback->percent_complete;
   std::cout<<"feedback"<<r;
@@ -96,23 +19,21 @@ float feedbackCb(const DoDishesFeedbackConstPtr& feedback)
 
 int main (int argc, char **argv)
 {
-  ros::init(argc, argv, "do_dishes_client");
+  ros::init(argc, argv, "product_client");
 
-  // Create the action client
-  Client ac("do_dishes", true);
+  Client ac("product", true);
 
   ROS_INFO("Waiting for action server to start.");
   ac.waitForServer();
   ROS_INFO("Action server started, sending goal.");
 
-  sewts_task::DoDishesGoal goal;
-  sewts_task::DoDishesResult result;
-  sewts_task::DoDishesFeedback feedback;
+  sewts_task::ProductSearchGoal goal;
+  sewts_task::ProductSearchResult result;
+  sewts_task::ProductSearchFeedback feedback;
 
   int d,e;
   bool ex=true;
-//float s;
-  // Fill in goal here
+
   do
   {
   
@@ -123,14 +44,15 @@ int main (int argc, char **argv)
   //ac.sendGoal(goal,&feedbackCb);
   ac.waitForResult(ros::Duration(5.0));
   //ac.getResult();
-  //DoDishesFeedbackConstPtr feedback;
+  //ProductSearchFeedbackConstPtr feedback;
   bool availability_product=ac.getResult()->result_presence_of_product;
-  //printf(ac.getResult()->percent_complete);
 
   if (availability_product==true)
   {d=0;
   goal.goal_check_product=d;
-  std::cout<<"Object found\nPress 1 for check Price\nPress 2 for check rating\nPress 3 for check weight\nPlease press desire button: ";
+  std::cout<<"--------------------------------------------------------------------------------------------------------------------------"<<std::endl;
+  std::cout<<"Object found\nPress 1 for check Price\nPress 2 for check weight\nPlease press desire button\n";
+  std::cout<<"--------------------------------------------------------------------------------------------------------------------------"<<std::endl;
   std::cin>>e;
   goal.goal_check_product_description=e;
   ac.sendGoal(goal);
@@ -139,16 +61,17 @@ int main (int argc, char **argv)
   
   int s=ac.getResult()->result_product_description;
   if (s==0)
-  {std::cout<<"User given wrong val:  "<< s <<std::endl;
+  {std::cout<<"User pressed wrong product description button !!!!"<<std::endl;
+   std::cout<<"Price -- 1, Weight -- 2"<<std::endl;
   }
   else
-  {std::cout<<"------------got price  "<< s <<std::endl;
+  {std::cout<<"Desired description value is: "<< s <<std::endl;
   }
 //  std::cout<<"got price  "<< s <<std::endl;
   e=0;
   goal.goal_check_product_description=e;
   char exit;
-  std::cout<<"Would you like to quit,press y/n";
+  std::cout<<"Would you like to quit,press y/n: ";
   std::cin>>exit;
   if (exit=='y')
   {
@@ -157,19 +80,9 @@ int main (int argc, char **argv)
   } 
   }
   else{
-   std::cout<<"Please provide correct product ID"<<std::endl;
+   std::cout<<"Please provide correct Product ID"<<std::endl;
   }
   }while((ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED || ex) && ex);
-  /*if (s == 15.0)
-    printf("Ok\n");
-  ros::spin();
-  if (s != 15.0)
-    printf("check the type");
-  if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    printf("Yay! The dishes are now clean");
-  printf("Current State: %s\n", ac.getState().toString().c_str());*/
-
-
 
   return 0;
 }
