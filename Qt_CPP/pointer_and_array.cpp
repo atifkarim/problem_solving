45b17aaffd3a819a6d588ef8c49ef61bf3f26b5d
atifkarim <atif0902082@gmail.com>
2020-03-27 11:47:19 +0100

    concept of array and pointer

---
+++ Qt_CPP/pointer_and_array.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int scores [] {100,85,95};
+
+    int * scores_ptr {scores};
+
+    cout<<"value printing from array"<<endl;
+    for (int i=0; i<3;i++){
+        cout<<scores[i]<<" "<<endl;
+    }
+
+    cout<<"\nvalue printing from POINTER pointed to array"<<endl;
+    // It is different from the pointer approach. Look it is not dereferencing rather it is a approach to use pointer as array
+
+    for (int i=0; i<3;i++){
+        cout<<scores_ptr[i]<<" "<<endl;
+    }
+    cout<<endl;
+
+    cout<<"Accessing array value by dereferencing POINTER"<<endl;
+
+    for (int i=0; i<3;i++){
+        cout<<*(scores_ptr+i)<<" "<<endl;
+    }
+
+    cout<<"value of POINTER address and Memory address of Array"<<endl;
+    cout<<"Pointer Address"<<"********"<<"Memory Adress of Array"<<endl;
+    for (int i=0; i<3;i++){
+        cout<<(scores_ptr+i)<<"*********"<<&(scores[i])<<endl;
+    }
+
+    cout<<"----------------LOOK THE FOLLOWING CODE IS COMPLETELY AS LIKE AS POINTER--------------------------------------"<<endl;
+    for (int i=0; i<3;i++){
+        cout<<*(scores+i)<<endl;
+    }
+
+    return 0;
+}
