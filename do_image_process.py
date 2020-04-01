8ff4de3f61c8218c03088dd5706da3082aa327ba
atifkarim <atif0902082@gmail.com>
2020-04-01 18:50:03 +0200

    shebang line for venv

---
+++ do_image_process.py
@@ -0,0 +1,15 @@
+#!/home/atif/myenv/bin/python3
+
+import cv2
+import matplotlib.pyplot as plt
+
+path = "/media/atif/BE0E05910E0543BD/University of Bremen MSc/problem_solving/photo.jpg"
+
+frame = cv2.imread(path)
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+print(frame)
+
+# cv2.imshow('hsv',hsv)
+# if cv2.waitKey() == ord('q'):
+#     cv2.destroyAllWindows()
