8895f1c171906b235d073a8bfab2cbdf2b2a5254
atifkarim <atif0902082@gmail.com>
2020-04-08 21:04:37 +0200

    object pass as argument of a function

---
+++ Qt_CPP/class_obj_as_argument_of_function.cpp
@@ -0,0 +1,47 @@
+/*
+Use object of class as an argument of function
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Summation{
+    private: 
+        int a; int b;
+    public:
+        Summation(int, int); // constructor declaration
+
+        // As usual function
+        int sum(int x, int y){
+            int d = x+y;
+            cout<<"op from public function SUM is: "<<d<<endl;
+            cout<<"value coming from private member: a: "<<a<<" b: "<<b<<" and sum is: "<<a+b<<endl;
+        ~Summation()
+            return d;
+        }
+};
+
+// constructor is declaring
+Summation::Summation(int q, int p){
+    cout<<"\nHey constrictor is called"<<endl;
+    a =q; b = p;
+}
+
+// The following function contains the definition whrere object from class will come as argument
+void obj_function(Summation obj_from_class){
+    int GetValue;
+    GetValue = obj_from_class.sum(6,5);
+}
+
+int main(){
+    int j = 4; int k = 3;
+    Summation obj_1(j,k);
+    obj_1.sum(3,2);
+
+    cout<<"now calling function which takes object as an argument"<<endl;
+
+    obj_function(obj_1);
+
+    return 0;
+}
