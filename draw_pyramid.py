f28da7939476077326798029a18e75c07042ec35
atifkarim <atif0902082@gmail.com>
2020-04-08 17:04:29 +0200

    recursive function

--- draw_pyramid.py
+++ draw_pyramid.py
@@ -1,4 +1,4 @@
-start_p = 1
+'''start_p = 1
 end_p = 20
 step_p = 1
 for i in range (start_p, end_p,step_p):
@@ -40,3 +40,29 @@ here_p = end_p
 for i in range(start_p, end_p, step_p):
     print(' '*i,here_p*'*')
     here_p = here_p-1
+
+'''
+
+x = float(input())
+x = int(abs(x))
+print('given val: ',x)
+
+i = 0
+
+def do_factorial(num):
+    global i
+    print('i: ',i)
+    if num == 0:
+        print('ami sesh vaiii: ', num)
+        return 1
+    elif num == 1:
+        print('ami ki asi: ',num)
+        return 1
+    else:
+        print('else val: ',num)
+        i = i+1
+        return num*do_factorial(num-1)
+
+d = do_factorial(x)
+print("i after 16 line: ",i)
+print('res is: ',d)
