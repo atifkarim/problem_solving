7b59d4d3a571732bb462488f7bb0dd52eb03c0f3
atifkarim <atif0902082@gmail.com>
2020-04-15 08:03:58 +0200

    A practise session to incerement array content by pointer

---
+++ Qt_CPP/practise_pointer_group.cpp
@@ -0,0 +1,138 @@
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+
+typedef vector<int> Array; // a declaration of a 1D vactor whose name is Array
+typedef vector<Array> Matrix; // a declaration of a 1D vactor whose name is Matrix
+
+int main(){
+    int row, column;
+    cin >> row; cin>> column;
+    int matrix [row][column];
+
+    int * my_pointer;
+
+    my_pointer = *matrix;
+    cout<<&matrix[0][0]<<endl;
+    cout<<my_pointer<<endl;
+    
+
+    for (int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cin>>*my_pointer;
+            my_pointer++;
+        }
+    }
+
+    cout<<"N: "<<my_pointer<<endl;
+
+   for (int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+    cout<<"\n";
+    }
+
+    cout<<"----------------------------------"<<endl;
+
+    
+    
+
+
+    return 0;
+}
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int * pointer_func(int * o, int row, int col){
+int a = 0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            // cin>>*((int *)ptr +i*col+j);
+            // cin>>*((int *)ptr +a);
+            cin>>*(o +a);
+            a++;
+
+            // cout<<"done";
+        }
+    }
+
+        return o;
+    }
+
+int main()
+{
+    int a=0;
+    int row=2;
+    int col=2;
+    int *ptr;
+    
+    
+    int matrix[row][col];
+    ptr=*matrix;
+
+    cout<<"ptr: "<<ptr<<endl;
+    
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            // cin>>*((int *)ptr +i*col+j);
+            // cin>>*((int *)ptr +a);
+            cin>>*(ptr +a);
+            cout<<"Now: "<<matrix[i][j]<<endl;
+            a++;
+
+            // cout<<"done";
+        }
+    }
+
+    cout<<"Now: ptr: "<<ptr<<endl;
+    
+    cout<<endl;
+    for (int i =0;i<row;i++){
+        for(int j =0; j<col; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<"\n";}
+
+
+    cout<<"-----------------------"<<endl;
+int b {0};
+    for (int s =0;s<row;s++){
+        for(int t=0;t<col;t++){
+            cout<<*(ptr+b)<<" ";
+            b++;
+        }
+   cout<<endl;
+    }
+
+    cout<<"#########################################"<<endl;
+
+    for(int h=0;h<row*col;h++){
+     cout<<*((int*)ptr+h);
+
+}
+cout<<"#########################################"<<endl;
+
+    int TOTAL_CELLS = row*col;
+    for (int i = 0; i < TOTAL_CELLS; i++) {
+    // printf("%d ", *(ptr + i));
+    cout<<*(ptr+i)<<" ";
+  }
+
+  cout <<"~~~~~~~~~~~~~~~~~~~~ calling function ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+
+  int  * z = pointer_func(ptr, row, col);
+   
+   int TOTAL_CELLS_1 = row*col;
+    for (int i = 0; i < TOTAL_CELLS_1; i++) {
+    // printf("%d ", *(ptr + i));
+    cout<<*(z+i)<<" ";
+  }
+   
+   return 0;
+}
