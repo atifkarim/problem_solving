d3955a749367ce8f4aae247aa57e56fe0fc0f5cc
atifkarim <atif0902082@gmail.com>
2020-04-15 07:55:45 +0200

    check for hsv and gray conversion

--- do_image_process.py
+++ do_image_process.py
@@ -2,14 +2,16 @@
 
 import cv2
 import matplotlib.pyplot as plt
-
+dir = "/media/atif/BE0E05910E0543BD/University of Bremen MSc/problem_solving/"
 path = "/media/atif/BE0E05910E0543BD/University of Bremen MSc/problem_solving/photo.jpg"
 
 frame = cv2.imread(path)
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+cv2.imwrite(dir+'cv2_image.png',img_gray)
 
 print(frame)
 
-# cv2.imshow('hsv',hsv)
-# if cv2.waitKey() == ord('q'):
-#     cv2.destroyAllWindows()
+cv2.imshow('GRAY',img_gray)
+if cv2.waitKey() == ord('q'):
+    cv2.destroyAllWindows()
