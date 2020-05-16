07fe1fbe332e30a99f121f8bdccf880e184cb827
atifkarim <atif0902082@gmail.com>
2020-05-16 03:19:16 +0200

    used pointer to pointer to return 2D array

--- Qt_CPP/return_2D_Array.cpp
+++ Qt_CPP/return_2D_Array.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<vector>
 
@@ -68,4 +69,71 @@ class ArrayInfo{
 
       return 0;
   }
+  */
+
+  #include <cstdio>
+  #include<iostream>
+  using namespace std;
+
+    // Returns a pointer to a newly created 2d array the array2D has size [height x width]
+
+    int** create2DArray(unsigned height, unsigned width)
+    {
+      int** array2D = 0;
+      array2D = new int*[height];
+
+      for (int h = 0; h < height; h++)
+      {
+            array2D[h] = new int[width];
+
+            for (int w = 0; w < width; w++)
+            {
+                  // fill in some initial values
+                  // (filling in zeros would be more logic, but this is just for the example)
+                  array2D[h][w] = w + width * h;
+            }
+      }
+      int *ptr{nullptr};
+      ptr = *array2D;
+      cout<<"mem address in fucn: "<<&array2D[0][0]<<" ,ptr mem address: "<<ptr<<endl;
+      return array2D;
+    }
+
+    int main()
+    {
+      printf("Creating a 2D array2D\n");
+      printf("\n");
+
+      int height = 15;
+      int width = 10;
+      int** my2DArray = create2DArray(height, width);
+      cout<<"mem address: "<<my2DArray<<endl;
+
+      printf("Array sized [%i,%i] created.\n\n", height, width);
+
+      // print contents of the array2D
+      printf("Array contents: \n");
+
+      for (int h = 0; h < height; h++)
+      {
+            for (int w = 0; w < width; w++)
+            {
+                  printf("%i,", my2DArray[h][w]);
+            }
+            printf("\n");
+      }
+
+          // important: clean up memory
+          printf("\n");
+          printf("Cleaning up memory...\n");
+          for (int  h = 0; h < height; h++)
+          {
+            delete [] my2DArray[h];
+          }
+          delete [] my2DArray;
+          my2DArray = 0;
+          printf("Ready.\n");
+
+      return 0;
+    }
 
