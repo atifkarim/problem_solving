5b0c0b937297afe5f3d0fe1018089d353ab60865
atifkarim <atif0902082@gmail.com>
2020-05-29 01:25:54 +0200

    no change

--- cpp_practise/template_basic.cpp
+++ cpp_practise/template_basic.cpp
@@ -3,16 +3,18 @@ First code to learn the usage of template in cpp. This code is taken from geeksf
 https://www.geeksforgeeks.org/templates-cpp/
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-template <typename T> // Here instead of typename you can also write class.
-T myMax(T x, T y){
-    return (x>y)?x:y;
+template <typename T> T myMax(T x, T y) // Here instead of typename you can also write class.
+{
+    return (x > y) ? x : y;
 }
 
-int main(){
-    cout<<myMax<int>(3,8)<<endl;
+int main()
+{
+    cout << myMax<int>(3, 8) << endl;
+    cout << myMax(93, 11) << endl;
     cout << myMax<double>(3.0, 7.0) << endl;
-    cout<<myMax<char>('e','t')<<endl;
+    cout << myMax<char>('e', 't') << endl;
     return 0;
 }
