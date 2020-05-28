cced30c20bb0ff45d3d4760410672b32a5b2ce69
atifkarim <atif0902082@gmail.com>
2020-05-28 16:33:37 +0200

    use referencing for base class

---
+++ cpp_practise/polymorphism_udemy_base_class_reference.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Account{
+    public:
+        virtual void withdraw(double amount){
+            cout<<"In Withdraw:: Base Class Account"<<endl;
+        }
+       virtual ~ Account(){cout<<"Account destructor is called"<<endl;}
+};
+
+class Savings:public Account{
+
+    public:
+        virtual void withdraw(double amount){
+            cout<<"In Withdraw:: Derived Class Savings"<<endl;
+        }
+
+       virtual ~ Savings(){cout<<"Savings destructor is called"<<endl;}
+};
+
+class Checking:public Account{
+    public:
+        virtual void withdraw(double amount){
+            cout<<"In Withdraw:: Derived Class Checking"<<endl;
+        }
+
+       virtual ~ Checking(){cout<<"Checking destructor is called"<<endl;}
+};
+
+class Trust:public Account{
+    public:
+        virtual void withdraw(double amount){
+            cout<<"In Withdraw:: Derived Class Trust"<<endl;
+        }
+
+       virtual ~ Trust(){cout<<"Trust destructor is called"<<endl;}
+};
+
+void do_withdraw(Account &account, double amount){
+    account.withdraw(amount);
+}
+
+int main(){
+    Account a;
+    Account &ref = a;
+    ref.withdraw(1000);
+    
+
+    Trust t;
+    // t.withdraw(89.90);
+    Account &ref_t = t;
+    ref_t.withdraw(500);
+
+    return 0;
+}
