449eff695b2e757c292709ced763077f9ede0595
atifkarim <atif0902082@gmail.com>
2020-05-28 09:17:47 +0200

    check with pointer object and static binding

--- cpp_practise/polymorphism_udemy_virtual_function.cpp
+++ cpp_practise/polymorphism_udemy_virtual_function.cpp
@@ -41,6 +41,50 @@ int main(){
     sav_obj.Account::withdraw(89.456); // And now here I have manipulated function's name.
                                     // If function's has same name then accessing will follow this rules
 
+    cout<<"\nPointer calling\n";
+    // We have seen from this piece of code the way of creating objets by using pointers
+    Account *p1 = new Account();
+    Savings *p2 = new Savings();
+    Checking *p3 = new Checking();
+    Trust *p4 = new Trust();
+
+    p1-> withdraw(56.98);
+    p2-> withdraw(56.98);
+    p3-> withdraw(56.98);
+    p4-> withdraw(56.98);
+
+    // free the memory by delete method
+    delete p1;
+    delete p2;
+    delete p3;
+    delete p4;
+
+    cout<<"\nPointer calling with static binding\n";
+    // We have seen from this piece of code the way of creating objets by using pointers
+    Account *p1_1 = new Account();
+    Account *p2_1 = new Savings();
+    Account *p3_1 = new Checking();
+    Account *p4_1 = new Trust();
+
+    p1_1-> withdraw(56.98);
+    p2_1-> withdraw(56.98);
+    p3_1-> withdraw(56.98);
+    p4_1-> withdraw(56.98);
+
+    // free the memory by delete method
+    delete p1_1;
+    delete p2_1;
+    delete p3_1;
+    delete p4_1;
+
+    cout<<"\nNow Base class Pointer is working. We will see the problem\n";
+    Account *acc_obj_1;
+    Savings sav_obj_1;
+    acc_obj_1 = &sav_obj_1;
+    acc_obj_1->withdraw(67.0); // Baseclass pointer is pointing to the address of savings class
+                            // which is a derived class but still it is printing message from the base class body
+
+
 
     return 0;
 }
