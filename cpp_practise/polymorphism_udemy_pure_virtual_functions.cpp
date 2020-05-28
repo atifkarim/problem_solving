a10de9b93fe4e3dd6b8efe38441852265ed9135a
atifkarim <atif0902082@gmail.com>
2020-05-28 23:29:40 +0200

    pure virtual, abstract class and concrete class example

---
+++ cpp_practise/polymorphism_udemy_pure_virtual_functions.cpp
@@ -0,0 +1,106 @@
+/*
+This code helps to learn about Pure Virtual Function and Abstract Class
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Shape{ // Abstract Base Class
+    private:
+
+    public:
+        virtual void draw() = 0; // pure virtual function
+        virtual void rotate() = 0; // pure virtual function
+        virtual ~ Shape(){cout<<"Abstract Base Class Shape's destructor is called\n";};
+
+};
+
+class Open_Shape:public Shape{ // Abstract Class
+    public:
+        virtual ~Open_Shape(){cout<<"Open_Shape destructor is called"<<endl;}
+};
+
+class Closed_Shape:public Shape{ // Abstract Class
+    public:
+        virtual ~Closed_Shape(){cout<<"Closed_Shape destructor is called"<<endl;}
+};
+
+class Line:public Open_Shape{
+    public:
+        virtual void draw() override{
+            cout<<"\nDrawing a line\n";
+        }
+
+        virtual void rotate() override{
+            cout<<"\nRotating a line\n";
+        }
+
+        virtual ~Line(){cout<<"Line destructor is called"<<endl;}
+};
+
+class Circle:public Closed_Shape{
+    public:
+        virtual void draw() override{
+            cout<<"Drawing a Circle\n";
+        }
+
+        virtual void rotate() override{
+            cout<<"Rotating a Circle\n";
+        }
+
+        virtual ~Circle(){cout<<"Circle destructor is called"<<endl;}
+};
+
+class Square:public Closed_Shape{
+    public:
+        virtual void draw() override{
+            cout<<"Drawing a Square\n";
+        }
+
+        virtual void rotate() override{
+            cout<<"Rotating a Square\n";
+        }
+
+        virtual ~Square(){cout<<"Square destructor is called"<<endl;}
+};
+
+
+void screen_refresh(const std::vector<Shape*> &shapes){
+    cout<<"Refreshing going on\n";
+    for (auto q:shapes)
+        q->draw();
+}
+
+int main(){
+    // Shape s; // Error
+    // Shape *ptr = new Shape(); // Error
+    
+    // Circle c;
+    // c.draw();
+
+    cout<<"\ndynamically handling all\n"<<endl;
+    /*
+    Shape *ptr = new Circle();
+    ptr->draw();
+    ptr->rotate();
+
+    delete ptr;
+   */
+
+    Shape *s1 = new Line();
+    Shape *s2 = new Circle();
+    Shape *s3 = new Square();
+
+    std::vector<Shape *> shapes {s1, s2, s3};
+    // for (auto p:shapes)
+    //     p->draw();
+
+
+
+    delete s1;
+    delete s2;
+    delete s3;
+
+    return 0;
+}
