b1f724affbf45c9caa3a3b312b5f6cfe4863af58
atifkarim <atif0902082@gmail.com>
2020-05-31 20:15:28 +0200

    smart pointer basic

---
+++ cpp_practise/unique_pointers/main.cpp
@@ -0,0 +1,38 @@
+/*
+This code helps us to know the use case of smart pointer
+*/
+
+#include<iostream>
+#include<memory>
+#include<vector>
+
+using namespace std;
+
+// #include "Account.h"
+// #include "Checking_Account.h"
+// #include "Trust_Account.h"
+// #include "Savings_Account.h"
+
+class Test{
+    private:
+        int data;
+    public:
+        Test(): data{0}{cout<<"Test constructor: "<<data<<endl;}
+        Test(int data):data{data}{cout<<"Here Test constructor: "<<data<<endl;}
+        int get_data() const {return data;}
+        ~Test(){cout<<"Destructor is called for data: "<<data<<endl;}
+};
+
+int main(){
+    Test t2; // No args constructor
+    Test t1 {800}; // Single args constructor
+
+
+    // Raw pointer which don't call destructor without using delete t3
+    Test *t3 = new Test {900};
+    delete t3;
+
+    std::unique_ptr<Test> t4{new Test {450}}; // Smart pointer whic don't require delete method to free the heap memory
+
+    return 0;
+}
