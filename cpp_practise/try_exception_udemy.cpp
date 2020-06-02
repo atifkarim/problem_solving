68172dadd371c8a598d96b743accfd663d4ba83c
atifkarim <atif0902082@gmail.com>
2020-06-02 18:36:08 +0200

    multiple exception

---
+++ cpp_practise/try_exception_udemy.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+double calculate_km_per_litre(int km_1, int litre_1){
+    if(litre_1 == 0)
+        throw 0;
+    return static_cast<double>(km_1)/litre_1;
+}
+
+int main(){
+    int km, litre;
+    cout<<"How many km you have travelled? : "; cin>> km;
+    cout<<"How much oil do you need in Litre? : ";cin>>litre;
+
+    cout<<"result km/litre is: "<< static_cast<double>(km)/litre<<endl; // Here if we omit static_cast<double>
+                                                                        // Then it will make error
+
+    // But the thing is we can also run the code flawlessly by using try and catch
+    
+    cout<<"Function calling"<<endl;
+    try{
+    double km_per_litre = calculate_km_per_litre(km, litre);
+    cout<<"resut from the function is: "<<km_per_litre<<endl;
+    }
+
+    catch(int &ex){
+
+        std::cerr<<"Tried to divide by zero: "<<&ex<<endl;
+    }
+
+    cout<<"Ending of the programm"<<endl;
+
+    return 0;
+
+}
