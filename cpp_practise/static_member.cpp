51e8a0ce8a10f5aa76bd8049fc58c2150234c9d4
atifkarim <atif0902082@gmail.com>
2020-06-05 17:44:48 +0200

    imp comments are added

--- cpp_practise/static_member.cpp
+++ cpp_practise/static_member.cpp
@@ -16,20 +16,21 @@ class static_class{
 
 };
 
-int static_class::x; // most impline for static member
-int static_class::y; // most impline for static member
+int static_class::x; // most imp line for static member
+int static_class::y; // most imp line for static member
 
 int main(){
 
     static_class smp1, smp2;
 
-    smp1.set_value(100,200);
-    smp1.print_info();
-    smp2.print_info();
+    smp1.set_value(100,200); // Using this line smp1 obj used to set the value of static variable x , y
+    smp1.print_info(); // So it can print
+    smp2.print_info(); // But look smp2 obj hasn't set any value but still can print because x, y is static 
+                        //and already got value by smp1
 
-    smp2.set_value(900,98);
-    smp1.print_info();
-    smp2.print_info();
+    smp2.set_value(900,98); // Now smp2 changed value of x,y
+    smp2.print_info(); // SO, printing
+    smp1.print_info(); // But look smp1 is now also printing and using the latest value
 
     return 0;
 }
