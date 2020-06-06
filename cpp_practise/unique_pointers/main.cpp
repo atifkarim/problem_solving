fb640149c41929647b9d705e44cf9d5e9a40ec00
atifkarim <atif0902082@gmail.com>
2020-06-06 16:46:28 +0200

    constructor arg changed, moved t4 to t6 and check with t6 to accessing the function

--- cpp_practise/unique_pointers/main.cpp
+++ cpp_practise/unique_pointers/main.cpp
@@ -18,7 +18,7 @@ class Test{
         int data;
     public:
         Test(): data{0}{cout<<"Test constructor: "<<data<<endl;}
-        Test(int data):data{data}{cout<<"Here Test constructor: "<<data<<endl;}
+        Test(int data_1):data{data_1}{cout<<"Here Test constructor: "<<data<<endl;}
         int get_data() const {return data;}
         ~Test(){cout<<"Destructor is called for data: "<<data<<endl;}
 };
@@ -36,10 +36,16 @@ int main(){
     
     std::unique_ptr<Test> t5 = std::make_unique<Test> (1400); // c++14 style smart pointer
 
+    cout<<"Accessing get_data fucntion by t4 : "<<t4->get_data ()<<endl;
+    
+
     // cannot copy of equal smart pointer
     std::unique_ptr<Test> t6;
     t6 = std::move(t4);
     if (!t4)
         cout<<"t4 is a null pointer"<<endl;
+    
+    cout<<"After moving t4 to t6 Accessing get_data fucntion by t6 : "<<t6->get_data ()<<endl;
+    // cout<<"Now check t4 exist or not to access data from the function: "<<t4->get_data ()<<endl;// Error will come
     return 0;
 }
