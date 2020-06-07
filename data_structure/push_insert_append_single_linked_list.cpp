f4358fa88061255e6eaf8471d35e70c64e989998
atifkarim <atif0902082@gmail.com>
2020-06-07 12:02:54 +0200

    insert node at the beginning2

--- data_structure/push_insert_append_single_linked_list.cpp
+++ data_structure/push_insert_append_single_linked_list.cpp
@@ -21,6 +21,48 @@ void PrintLinkedList(node *n){
     }
 }
 
+// following function helps to add new data at the beginning of the previous linked list
+
+void push(node** head_ref, int new_data)  
+{  
+    /* 1. allocate node */
+    node* new_node = new node(); 
+  
+    /* 2. put in the data */
+    new_node->data = new_data;  
+  
+    /* 3. Make next of new node as head */
+    new_node->next = (*head_ref);  // doing the connection of new cotainer's next with just
+                                    // the previous container's head. And then transfer the head
+                                    // of the given linked list's to the new container's pointing location
+                                    // which is pointed by new_node. Next line has done it.
+  
+    /* 4. move the head to point to the new node */
+    (*head_ref) = new_node;  
+}
+
+void insertAfter(node* prev_node, int new_data)  
+{  
+    /*1. check if the given prev_node is NULL */
+    if (prev_node == NULL)  
+    {  
+        cout<<"the given previous node cannot be NULL";  
+        return;  
+    }  
+  
+    /* 2. allocate new node */
+    node* new_node = new node(); 
+  
+    /* 3. put in the data */
+    new_node->data = new_data;  
+  
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;  
+  
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = new_node;  
+}  
+
 // following function helps to add new data in the end of the previous linked list
 void append(node** head_ref, int new_data)  
 {  
@@ -100,6 +142,8 @@ int main(){
     n -> next =  NULL; // This is telling to stop the creation of new node
 
     append(&h, 6);
+    push(&h, 87);
+    insertAfter(h->next->next, 8);
 
 
 
