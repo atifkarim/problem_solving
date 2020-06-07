de3cc65249136e153f01f206775d65117147003f
atifkarim <atif0902082@gmail.com>
2020-06-07 16:28:05 +0200

    given another source to learn enum

---
+++ cpp_practise/enum.cpp
@@ -0,0 +1,26 @@
+// Program to demonstrate working 
+// of enum in C++ 
+// https://www.geeksforgeeks.org/user-defined-data-types-in-c/
+// https://www.geeksforgeeks.org/enumeration-enum-c/
+
+#include <iostream> 
+using namespace std; 
+
+enum week { Mon, 
+			Tue, 
+			Wed, 
+			Thur, 
+			Fri, 
+			Sat, 
+			Sun }; 
+// enum iam {Mon};  // will show error as in the previous declaration it is present
+int main() 
+{ 
+	enum week day; 
+
+	day = Wed; 
+
+	cout << day; 
+
+	return 0; 
+} 
