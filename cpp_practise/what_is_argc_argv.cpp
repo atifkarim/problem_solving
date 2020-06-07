b1b88cf7d46ed8aa841ce758fc2612b649e3722f
atifkarim <atif0902082@gmail.com>
2020-06-07 16:28:25 +0200

    learning argc and argv

---
+++ cpp_practise/what_is_argc_argv.cpp
@@ -0,0 +1,19 @@
+/*
+A common way of writing cpp code sometimes holds the args name argc and argv. Lets dig themselves
+https://www.youtube.com/watch?v=aP1ijjeZc24
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+    // The following line will say how many args is/are passed during the running of the code
+    // after compiling the code I have a executable file, lets suppose, it is "main"
+    // To run this, I write in terminal ./main
+    // At this time the following line will print : 1
+    // But if I write ./main atif bin karim
+    // then it will print 4
+    // ./main itself an argument so it prints 1 at the first time.
+    cout<<"argc is here %d\n"<<argc<<endl;
+    return 0;
+}
