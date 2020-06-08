1ff2502feefa1870cfe49f7789efba526d12a53d
atifkarim <atif0902082@gmail.com>
2020-06-08 17:25:46 +0200

    this code will show error as class D inherits class A from B and C. So compiler doesn't know from where it will take the data

--- cpp_practise/virtual_base_class.cpp
+++ cpp_practise/virtual_base_class.cpp
@@ -7,5 +7,51 @@ If right now Class D want to access any member of Class of A then ambiguity prob
 which meand that compiler cannot take decision which A it will take(from Class B OR Class C)
 To remove this virtual Base Class concept used
 source : https://www.geeksforgeeks.org/virtual-base-class-in-c/
-
 */
+#include<iostream>
+using namespace std;
+
+class A{
+    public:
+        int i;
+        void PrintA(){
+            cout<<"I am printing the value of i from class A "<<i<<endl;
+        }
+};
+
+class B:public A{
+    public:
+        void PrintB(){
+            cout<<"I am the Class B who is child of class A and i know that value of i in A is: "<<i<<endl;
+        }
+};
+
+class C:public A{
+    public:
+        void PrintC(){
+            cout<<"I am Class C who is the child of class A and i know that value of i in A is: "<<i<<endl;
+        }
+};
+
+// Now creating Class D who will inherit both Class B and C // multiple inheritance
+
+class D:public B,public C{
+    public:
+        void PrintD(){
+            cout<<"I am the class who possess the multiple inheritance and I know the the val of i in A is: "<<i<<endl;
+        }
+
+};
+
+int main(){
+    B obj_b;
+    obj_b.i = 15;
+    obj_b.PrintB();
+    obj_b.PrintA();
+
+    cout<<"\nNow the part of class D\n";
+    D obj_d;
+    obj_d.PrintD();
+    
+    return 0;
+}
