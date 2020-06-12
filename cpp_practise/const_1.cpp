ab6565f5347206b034d9b34d96a2488b4fe438a9
atifkarim <atif0902082@gmail.com>
2020-06-12 22:11:41 +0200

    a source to learn const in cpp

---
+++ cpp_practise/const_1.cpp
@@ -0,0 +1,28 @@
+/*
+const in cpp
+http://people.cs.aau.dk/~normark/ap/basic-facilities-slide-const-reference.html
+*/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+typedef int T;
+
+const T& f(double d){
+//   double e = 2 * d;
+//   return e;                      // Compiler warning:
+                                 // returning reference to temporary
+d = 9.5;
+return d;
+}
+
+int main(){
+  double expression = 5.3;
+
+  const T &var = f(expression);  
+                                 
+  cout << var << endl;           // ??
+}
