43a9952c776aff9a8b880473cca2fc5a17f609c2
atifkarim <atif0902082@gmail.com>
2020-06-13 12:26:34 +0200

    functor introduced to thread

--- cpp_practise/multithreading_different_ways.cpp
+++ cpp_practise/multithreading_different_ways.cpp
@@ -1,5 +1,6 @@
 /*
 There are diffrent ways of making threading in cpp
+https://www.youtube.com/watch?v=hCvc9y39RDw&list=PLk6CEY9XxSIAeK-EAh3hB4fgNvYkYmghp&index=2
 */
 
 
@@ -29,18 +30,18 @@ int main(){
 */
 
 /******************************************Lambda Function******************************************/
-
-int main(){
 /*
-    auto fun = [](int x){ 
+int main(){
 
-        while(x-->0){
-            cout<<"the val is: "<<x<<endl;
-        }
-    };
+    // auto fun = [](int x){ 
 
-    std::thread t (fun, 10);
-    */
+    //     while(x-->0){
+    //         cout<<"the val is: "<<x<<endl;
+    //     }
+    // };
+
+    // std::thread t (fun, 10);
+    
 
     // now just comment the lambda function to threading and uncomment the following lines to inject lambda
     // directly at thread creation time
@@ -53,5 +54,25 @@ int main(){
     }, 10);
     t.join();
 
+    return 0;
+}
+
+*/
+
+
+/******************************************Functor******************************************/
+
+class Base{
+    public:
+        void operator ()(int x){ // keep the name operator. function object
+            while(x-->0){
+                cout<<"val is now: "<<x<<endl;
+            }
+        }
+};
+
+int main(){
+    std::thread t ( (Base()), 10);
+    t.join();
     return 0;
 }
