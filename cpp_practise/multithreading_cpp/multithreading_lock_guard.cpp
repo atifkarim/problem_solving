de2720001d65047b08b8fa91fc37c805c10743c6
atifkarim <atif0902082@gmail.com>
2020-06-14 17:19:16 +0200

    lock guard for locking

---
+++ cpp_practise/multithreading_cpp/multithreading_lock_guard.cpp
@@ -0,0 +1,31 @@
+/**/
+
+
+#include <iostream>
+#include <thread>
+#include<mutex>
+
+using namespace std;
+
+int buffer = 0;
+std::mutex m;
+
+void task(const char* threadNumber, int loopFor){
+    std::lock_guard<mutex> lock(m);
+    for(int i =0; i<loopFor; ++i){
+        buffer++;
+        cout<<threadNumber<<" : "<<buffer<<endl;
+    }
+
+}
+
+int main(){
+
+    std::thread t1(task, "T1", 10);
+    std::thread t2(task, "T2", 10);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
