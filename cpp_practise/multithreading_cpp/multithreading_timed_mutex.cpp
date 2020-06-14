104d73663bd34740dda7e9757a891f59a23990b6
atifkarim <atif0902082@gmail.com>
2020-06-14 12:22:04 +0200

    time mutex with try_lock_until

--- cpp_practise/multithreading_cpp/multithreading_timed_mutex.cpp
+++ cpp_practise/multithreading_cpp/multithreading_timed_mutex.cpp
@@ -27,11 +27,25 @@ void increment_try_lock_for (int i){
     }
 }
 
+/*try lock until*/
+
+void increment_try_lock_until (int i){
+    auto now = std::chrono::steady_clock::now();
+    if (m.try_lock_until(now + std::chrono::seconds(1))){
+        ++myAmount;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        cout<<"Thread "<<i<<" entered"<<endl;
+        m.unlock();
+    }
+    else{
+        cout<<"Thread "<<i<<" Couldn't Enter"<<endl;
+    }
+}
 
 int main(){
 
-    std::thread t1(increment_try_lock_for,1);
-    std::thread t2(increment_try_lock_for,2);
+    std::thread t1(increment_try_lock_until,1);
+    std::thread t2(increment_try_lock_until,2);
 
     t1.join();
     t2.join();
