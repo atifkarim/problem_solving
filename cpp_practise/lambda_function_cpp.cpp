44015c8f8183abd37300635baae887fcdde0bf91
atifkarim <atif0902082@gmail.com>
2020-06-18 09:58:07 +0200

    pass by value of local variable to the lambda function

--- cpp_practise/lambda_function_cpp.cpp
+++ cpp_practise/lambda_function_cpp.cpp
@@ -28,5 +28,18 @@ int main(){
 
     cout<<"Result of add function is: "<<add(200,15)<<endl;
 
+    // It is not possible to use any local variable in the bosy of the lambda function though they
+    // are the under the same scope. But for this Capture list [] will work. Just pass the name of the variable
+    // But this will just copy the value, like a parameter passed by the value
+
+    int p = 10;
+    int q = 20;
+
+    auto add_1 = [p,q](int a , int b) {
+        return a + b + p + q;
+    };
+
+    cout<<"Result of add_1 function is: "<<add_1(200,10)<<endl;
+
 
 }
