1553d262bacc89e9e7c9804f1c0d3d61c3576461
atifkarim <atif0902082@gmail.com>
2020-06-28 18:18:14 +0200

    bitwose operatiron 2

---
+++ cpp_practise/bitwise_operator_1.cpp
@@ -0,0 +1,13 @@
+/*
+Many things learned from Niton Bangla book , C related
+*/
+
+#include <bitset>
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Ans is: "<<(15&&127)<<endl; // here && means logica operator and ans will be 1. if a single & is used
+                                        // then it will be bitwise AND
+    return 0;
+}
