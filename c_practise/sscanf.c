8bf4c1e34ff2d62fe6c624253885824220d3b07f
atifkarim <atif0902082@gmail.com>
2020-07-04 21:45:17 +0200

    use case of sscanf

---
+++ c_practise/sscanf.c
@@ -0,0 +1,22 @@
+/*
+https://overiq.com/c-programming-101/the-sscanf-function-in-c/
+*/
+
+#include<stdio.h>
+#include<string.h>
+ 
+int main()
+{
+    char *str = "Tom Manager 28";
+    char name[10], designation[10];
+    int age, ret;
+ 
+    ret = sscanf(str, "%s %s %d", name, designation, &age);
+ 
+    printf("Name: %s\n", name);
+    printf("Designation: %s\n", designation);
+    printf("Age: %d\n", age);
+ 
+    // signal to operating system program ran fine
+    return 0;
+}
