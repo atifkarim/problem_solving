eadb81cad566e29eab2df28c0ee91580ae8bfd39
atifkarim <atif0902082@gmail.com>
2020-08-17 23:15:45 +0200

    reverse of string in C

---
+++ c_practise/reverse_string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    // char *name = "Atif";
+    char name[30];
+    printf("\nPlease enter your string val: ");
+    gets(name);
+    printf("\nActual string is: %s", name);
+    // strrev(name);
+    printf("\nActual string is: %s", name);
+
+
+    printf("\n");
+
+    return 0;
+
+}
