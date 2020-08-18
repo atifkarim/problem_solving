df6ad656c7f62d5ccdbdf14dde2a36d78c00f55e
atifkarim <atif0902082@gmail.com>
2020-08-18 22:35:59 +0200

    dir for cmake learning is added

---
+++ project_cmake/hello_world/main.cpp
@@ -0,0 +1,14 @@
+/*
+source : https://riptutorial.com/Download/cmake.pdf
+this command used to make and run the code: cmake . && cmake --build . && ./app
+
+Initially there was only main.cpp and CmakeLists.txt file will present. Then all other files andf folders will
+come after the build
+*/
+
+#include <iostream>
+int main()
+{
+ std::cout << "Hello World!\n";
+ return 0;
+}
