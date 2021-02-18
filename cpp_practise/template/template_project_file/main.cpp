ec6e0b75bf37f28d9a5a13df3d7e81dc7528dd0a
atifkarim <atif0902082@gmail.com>
2021-02-18 22:07:57 +0100

    add source of temp file

--- cpp_practise/template/template_project_file/main.cpp
+++ cpp_practise/template/template_project_file/main.cpp
@@ -2,6 +2,7 @@
  * How to Define a Template Class in a .h File and Implement it in a .cpp File
  * https://www.codeproject.com/Articles/48575/How-to-Define-a-Template-Class-in-a-h-File-and-Imp
  * Method 2 taken from the above source
+ * imp: https://gist.github.com/mido3ds/8ff9e1283a8e1ffdb32b55f1d3e8133a#file-base-hpp
  */
 
 #include <iostream>
