2afddc35a06b47cd8a30ec67370f9cfa76b6c887
atifkarim <atif0902082@gmail.com>
2021-03-31 00:14:52 +0200

    enum type trying to fed in tuple

--- cpp_practise/Task_Enum_Struct.cpp
+++ cpp_practise/Task_Enum_Struct.cpp
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/321801/enum-c-get-by-index/321828
 # include <iostream>
 # include <vector>
 # include <tuple>
@@ -15,29 +16,50 @@ struct DataHolder{
 
     DataHolder(){}
 
-    void get1(this);
+    // void get1(this);
 
     void add_file_name(std::string name);
 
-    void add_text_line(DataStatus enum_val);
+    void add_text_line();
 };
 
 void DataHolder::add_file_name(std::string name)
 {
-
+    lines.push_back(name);
 }
 
-void DataHolder::add_text_line(DataStatus enum_val)
+void DataHolder::add_text_line()
 {
 
 }
 
+std::tuple<int , DataHolder> give_me_tuple(int index, DataHolder obj){
+    DataStatus myValue = (DataStatus)index;
+    return std::make_tuple(myValue, obj.lines[index]);
+}
+
 
 int main(){
-    std :: vector <std :: tuple <DataStatus , DataHolder>> dataOverview ;
+    enum DataStatus d;
+    d = Red;
+
+    using UserUndT = std::underlying_type<DataStatus>::type;
+
+    std :: vector <std :: tuple <int , DataHolder>> dataOverview ;
     std::string testText1 = "Add me";
     std::string testText2 = "Write me";
     std::cout<<"Hello\n";
+
+    DataHolder obj;
+    obj.add_file_name(testText1);
+    obj.add_file_name(testText2);
+
+    // for(int i = 0; i< obj.lines.size(); i++){
+    //     std::cout<<obj.lines[i]<<" ";
+    // }
+
+    std::tuple<int , DataHolder> first_tuple;
+    // first_tuple = std::make_tuple(DataStatus::Red , obj.lines[0]);
 
     // std::cout<<"Enum Red: "<<static_cast<int>DataStatus::Red<<std::endl;
 
