ed3ec033f4944d3547c6e7b1089c78fbc9464a59
atifkarim <atif0902082@gmail.com>
2021-10-09 23:25:04 +0200

    function name changed

--- cpp_practise/pointer_cpp/pointer_to_pointer_function_arg.cpp
+++ cpp_practise/pointer_cpp/pointer_to_pointer_function_arg.cpp
@@ -1,4 +1,4 @@
-/// This file will show hoe to send a pointer to a function as pointer arg
+/// This file will show how to send a pointer to a function as pointer arg
 /**
  * \command_to_execute : valgrind --leak-check=full --show-leak-kinds=all -v ./binary_file
  * source : https://www.geeksforgeeks.org/passing-reference-to-a-pointer-in-c/
@@ -14,10 +14,10 @@
  * op_ptr_array_ pointer points to that array
  * thus from the main function op_ptr_array pointer also points to that same value/ array
  **/
-void op_calculation(unsigned int*  ip_ptr_array_,
-                    unsigned int   ip_size_,
-                    unsigned int** op_ptr_array_,
-                    unsigned int*  op_size_)
+void algo_fun(unsigned int*  ip_ptr_array_,
+              unsigned int   ip_size_,
+              unsigned int** op_ptr_array_,
+              unsigned int*  op_size_)
 {
 	*(op_size_) = ip_size_ + 2;
 	// std::cout<<"op_size_ address: "<<op_size_<<std::endl;
@@ -28,16 +28,16 @@ void op_calculation(unsigned int*  ip_ptr_array_,
 
 	// following approach is good as it allocate dynamic memory
 	unsigned int* local = new unsigned int[*(op_size_)];
-	// std::cout<<"making local array to be pointed by op_ptr_array_\n";
+
 	for (unsigned int i = 0; i< *(op_size_); i++)
 	{
 		local[i]=i+1*3;
 	}
-	// std::cout<<"\n";
+
 	*op_ptr_array_ = &local[0];
 	local[3] = 87; // for checking pointer charecter
-	for (unsigned int i = 0; i < *(op_size_); i++)
-		std::cout<<"array address: "<<&local[i]<<" ,op_ptr_array address: "<<(*op_ptr_array_)+i<<" ,val of array: "<<local[i]<<" ,val at op_ptr_array: "<<*((*op_ptr_array_)+i)<<std::endl;
+	// for (unsigned int i = 0; i < *(op_size_); i++)
+	// 	std::cout<<"array address: "<<&local[i]<<" ,op_ptr_array address: "<<(*op_ptr_array_)+i<<" ,val of array: "<<local[i]<<" ,val at op_ptr_array: "<<*((*op_ptr_array_)+i)<<std::endl;
 }
 
 int main()
@@ -57,21 +57,10 @@ int main()
 	}
 
 	// function calling to get output data
-	op_calculation(ip_ptr_array,
+	algo_fun(ip_ptr_array,
 	               ip_size,
 	               &op_ptr_array,
 	               &op_size);
-	
-	// std::cout<<"Value printing after operation of op_calculation function\n";
-	// std::cout<<"op_size: "<<op_size<<std::endl;
-
-	// std::cout<<"op_ptr_array\n";
-	for(unsigned int i = 0; i < op_size; i++)
-		std::cout<<"Address: "<<(op_ptr_array+i)<<" , Value: "<<*(op_ptr_array+i)<<"\n";
-		/* Here only addresses are same with `local array` and
-		address pointed by op_ptr_array_ pointer` which you will find in op_calculation
-		function but values are different*/
-	std::cout<<"\n";
 
 	delete [] ip_ptr_array;
 	delete [] op_ptr_array;
